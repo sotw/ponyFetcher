3bac8347e44e40b4e462f669a9a0d078b817db43
Nostrum <workthethird@gmail.com>
2014-05-21 00:05:45 +0800

    try to figure out why bw 2/3 is not downloading...

--- perform.sh
+++ perform.sh
@@ -1,3 +1,4 @@
 #python ponyFetcher.py "http://hdx3.blogspot.com/2014/05/my-little-pony-friendship-is-magic_11.html" 1
-python ponyFetcher.py "http://hdx3.blogspot.tw/2011/06/regular-show-s02e24-more-smarter.html" 1
+#python ponyFetcher.py "http://hdx3.blogspot.tw/2011/06/regular-show-s02e24-more-smarter.html" 1
+python ponyFetcher.py "http://hdx3.blogspot.tw/2013/04/bravest-warriors-s01e0103_8.html" 1
 
