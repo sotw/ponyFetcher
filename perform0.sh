c12b6026d2bb87ced44a179847f61bfba0756bd5
Nostrum <workthethird@gmail.com>
2014-11-07 10:59:47 +0800

    polish perform0.sh

--- perform0.sh
+++ perform0.sh
@@ -16,53 +16,16 @@ function travelNode
 	fi
 }
 
-mkdir -p BraveWarrior
-python ponyFetcherList.py "http://hdx3.blogspot.com/search/label/Bravest%20Warriors?max-results=200" BraveWarrior 0
-if [ -x /usr/syno/bin/synoindex ]; then 
-   /usr/syno/bin/synoindex -R $PWD/BraveWarrior 
-fi
-mkdir -p RegularShow
-python ponyFetcherList.py "http://hdx3.blogspot.com/search/label/Regular%20Show?updated-max=2011-07-22T03:04:00-07:00&max-results=200&start=98&by-date=false" RegularShow 0 
-if [ -x /usr/syno/bin/synoindex ]; then 
-   /usr/syno/bin/synoindex -R $PWD/RegularShow 
-fi
-mkdir -p AdventureTime
-python ponyFetcherList.py "http://hdx3.blogspot.com/search/label/Adventure%20Time?updated-max=2011-09-05T03:41:00-07:00&max-results=200&start=104&by-date=false" AdventureTime 0
-if [ -x /usr/syno/bin/synoindex ]; then 
-   /usr/syno/bin/synoindex -R $PWD/AdventureTime 
-fi
-mkdir -p MyLittlePony
-python ponyFetcherList.py "http://hdx3.blogspot.com/search/label/My%20Little%20Pony?max-results=200" MyLittlePony 0
-if [ -x /usr/syno/bin/synoindex ]; then 
-   /usr/syno/bin/synoindex -R $PWD/MyLittlePony 
-fi
-mkdir -p RickAndMorty
-python ponyFetcherList.py "http://hdx3.blogspot.com/search/label/Rick%20and%20Morty?max-results=200" RickAndMorty 0
-if [ -x /usr/syno/bin/synoindex ]; then 
-   /usr/syno/bin/synoindex -R $PWD/RickAndMorty 
-fi
-mkdir -p DanVs
-python ponyFetcherList.py "http://hdx3.blogspot.com/search/label/Dan%20Vs.?max-results=200" DanVs 0
-if [ -x /usr/syno/bin/synoindex ]; then 
-   /usr/syno/bin/synoindex -R $PWD/DanVs 
-fi
-mkdir -p UglyAmericans
-python ponyFetcherList.py "http://hdx3.blogspot.com/search/label/Ugly%20Americans?max-results=200" UglyAmericans 0
-if [ -x /usr/syno/bin/synoindex ]; then 
-   /usr/syno/bin/synoindex -R $PWD/UglyAmericans 
-fi
-mkdir -p Metalocalypse
-python ponyFetcherList.py "http://hdx3.blogspot.com/search/label/Metalocalypse?max-results=200" Metalocalypse 0
-if [ -x /usr/syno/bin/synoindex ]; then 
-   /usr/syno/bin/synoindex -R $PWD/Metalocalypse 
-fi
-mkdir -p AVGN
-python ponyFetcherList.py "http://hornydragon.blogspot.com/search/label/AVGN?max-results=200" AVGN 0
-if [ -x /usr/syno/bin/synoindex ]; then 
-   /usr/syno/bin/synoindex -R $PWD/AVGN 
-fi
-mkdir -p SuperJail
-python ponyFetcherList.py "http://hdx3.blogspot.com/search/label/Superjail?max-results=200" SuperJail 0
-if [ -x /usr/syno/bin/synoindex ]; then 
-   /usr/syno/bin/synoindex -R $PWD/Superjail 
-fi
+bkIFS=$IFS
+IFS=$'\r\n'
+DATA=($(cat ./data))
+for line in ${DATA[@]}
+do
+	IFS=' ' 
+	read -a array <<< $line
+	echo ${array[0]}
+	echo ${array[1]}
+	travelNode ${array[0]} ${array[1]}
+	IFS=$'\r\n'
+done
+IFS=$bkIFS
